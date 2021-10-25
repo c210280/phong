cb6eb438f462155dbce804d6844393a36ef0675c
c210280 <91927121+c210280@users.noreply.github.com>
2021-10-25 09:29:29 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Mai {
+
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        System.out.println(args.length);
+        System.out.println(args[0]);
+
+        
+    }
+}
